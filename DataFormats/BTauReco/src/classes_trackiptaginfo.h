3b123d2067142620103875169fad17ee639e314e
Andrea <andrea.rizzi@cern.ch>
2014-07-31 12:28:10 +0200

    test dicts

--- DataFormats/BTauReco/src/classes_trackiptaginfo.h
+++ DataFormats/BTauReco/src/classes_trackiptaginfo.h
@@ -19,22 +19,34 @@
 #include "DataFormats/BTauReco/interface/TrackIPTagInfo.h"
 
 // Need a dictionary for class which was removed
+namespace reco{
 class TrackIPTagInfo {};
+class SecondaryVertexTagInfo {
+		public:
+		class TrackData {};
+		class VertexData {};
+	};
+}
 // Make it easy to distinguish old and new class using the same name
-namespace reco {namespace old { typedef ::reco::TrackIPTagInfo TrackIPTagInfo; } }
+namespace reco {namespace old {
+	 typedef ::reco::TrackIPTagInfo TrackIPTagInfo; 
+	 typedef ::reco::SecondaryVertexTagInfo SecondaryVertexTagInfo; 
+	 typedef std::vector<TrackIPTagInfo> TrackIPTagInfoCollection;
+	 typedef std::vector<SecondaryVertexTagInfo> SecondaryVertexTagInfoCollection;
+} }
 
 namespace DataFormats_BTauReco {
   struct dictionarytrackiptaginfo {
 
     reco::old::TrackIPTagInfo                                           otcip;
     reco::old::TrackIPTagInfoCollection                                 otcip_c;
-    reco::old::TrackIPTagInfoRef                                        otcip_r;
-    reco::old::TrackIPTagInfoFwdRef                                     otcip_fr;
-    reco::old::TrackIPTagInfoRefProd                                    otcip_rp;
-    reco::old::TrackIPTagInfoRefVector                                  otcip_rv;
-
     edm::Wrapper<reco::old::TrackIPTagInfoCollection>                   otcip_wc;
-
+    reco::old::SecondaryVertexTagInfo::TrackData			svtdold;
+    reco::old::SecondaryVertexTagInfo::VertexData			svvdold;
+    reco::old::SecondaryVertexTagInfo                                   otcipsv;
+    reco::old::SecondaryVertexTagInfoCollection                            otcip_csv;
+    edm::Wrapper<reco::old::SecondaryVertexTagInfoCollection>                  otcip_wcsv;
+	
     reco::nw::TrackIPTagInfo                                            ntcip;
     reco::nw::TrackIPTagInfoCollection                                  ntcip_c;
     reco::nw::TrackIPTagInfoRef                                         ntcip_r;
@@ -44,13 +56,9 @@ namespace DataFormats_BTauReco {
 
     edm::Wrapper<reco::nw::TrackIPTagInfoCollection>                    ntcip_wc;
 
-    edm::reftobase::Holder<reco::BaseTagInfo, reco::old::TrackIPTagInfoRef>          rb_otcip;
-    edm::reftobase::RefHolder<reco::old::TrackIPTagInfoRef>                          rbh_otcip;
     edm::reftobase::Holder<reco::BaseTagInfo, reco::nw::TrackIPTagInfoRef>           rb_ntcip;
     edm::reftobase::RefHolder<reco::nw::TrackIPTagInfoRef>                           rbh_ntcip;
 
-    edm::reftobase::Holder<reco::BaseTagInfo, reco::old::TrackIPTagInfoFwdRef>       rb_otcipf;
-    edm::reftobase::RefHolder<reco::old::TrackIPTagInfoFwdRef>                       rbh_otcipf;
     edm::reftobase::Holder<reco::BaseTagInfo, reco::nw::TrackIPTagInfoFwdRef>        rb_ntcipf;
     edm::reftobase::RefHolder<reco::nw::TrackIPTagInfoFwdRef>                        rbh_ntcipf;
 
