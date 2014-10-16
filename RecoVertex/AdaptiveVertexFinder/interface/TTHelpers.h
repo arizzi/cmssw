03614887972605352cd93b2a521adb4bf121f46c
Andrea <andrea.rizzi@cern.ch>
2014-10-16 12:18:33 +0200

    debug

--- RecoVertex/AdaptiveVertexFinder/interface/TTHelpers.h
+++ RecoVertex/AdaptiveVertexFinder/interface/TTHelpers.h
@@ -9,6 +9,10 @@ inline reco::TransientTrack buildTT(edm::Handle<reco::TrackCollection> & tracks,
 inline reco::TransientTrack buildTT(edm::Handle<edm::View<reco::Candidate> > & tracks, edm::ESHandle<TransientTrackBuilder> &trackbuilder, unsigned int k)
 {
 	if((*tracks)[k].bestTrack() == 0) return reco::TransientTrack();
+	if((*tracks)[k].pdgId() == 310)	
+	{
+		std::cout << "K0 " << (*tracks)[k].bestTrack()->pt() << std::endl;
+	}
         return trackbuilder->build(tracks->ptrAt(k));
 }
 }
